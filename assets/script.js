4c03075c94615a5e4dc95ab189a127df70b85e31
“Kevin <kecualos22@gmail.com>
2022-01-24 21:20:50 -0500

    Finished assessment

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,65 @@
-// HINT: You can delete this console.log after you no longer need it!
 console.log('JavaScript code has loaded!')
 // First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+let yourName = "Kevin Cuesta" // HINT: Replace this with your own name!
 
 // We'll use these variables to track the counts of each cookie type
 let gb = 0 // Gingerbread
 let cc = 0 // Chocolate Chip
 let sugar = 0 // Sugar Sprinkle
 
-// selecting the element with an id of credit
+/** 
+ * get quantity 
+ * if plus button clicked , increment quantity
+ * if negative button clicked, subtract quantity and check that quantity is not less than 0
+ */
+
 const credit = document.querySelector('#credit')
-// selecting the element with an id of add-gb
 const gbPlusBtn = document.querySelector('#add-gb')
-
-// Code to update name display
-credit.textContent = `Created by ${yourName}`
-
-// Event listener for clicks on the "+" button for Gingerbread cookies
+let positivegbBtn = document.getElementById('quantity-up');
 gbPlusBtn.addEventListener('click', function() {
-// HINT: You can delete this console.log after you no longer need it!
+    quantity = quantity +1;
+    quantityContainer.textContent = `Quantity: ${quantity}`;
+});
+
+let negativegbBtn = document.addEventListener('quantity-down');
+negativegbBtn.addEventListener('click', function(){
+    if(quantity > 0) {
+        quantity = quantity - 1;
+    }
+    quantityContainer.textContent = `Quantity: ${quantity}`;
+});
 console.log('Gingerbread + button was clicked!')
 
-// TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
-})
+const ccPlusBtn = document.querySelector('#add-cc')
+let positiveccBtn = document.getElementById('quantity-up');
+ccPlusBtn.addEventListener('click', function() {
+    quantity = quantity +1;
+    quantityContainer.textContent = `Quantity: ${quantity}`;
+});
+let negativeccBtn = document.getElementById('quantity-down');
+negativeccBtn.addEventListener('click', function(){
+    if(quantity > 0) {
+        quantity = quantity - 1;
+    }
+    quantityContainer.textContent = `Quantity: ${quantity}`;
+});
+console.log('Chocolate + button was clicked!')
 
-// TODO: Hook up event listeners for the rest of the buttons
+const credit = document.querySelector('#credit')
+const sugarPlusBtn = document.querySelector(#add-sugar)
+let positivesugarBtn = document.getElementById('quantity-up');
+sugarPlusBtn.addEventListener('click', function(){
+    quantity = quantity +1;
+    quantityContainer.textContent = `Quantity: ${quantity}`;
+});
+
+let negativesugarBtn = document.getElementById('quantity-down');
+negativesugarBtn.addEventListener('click', function(){
+    if(quantity > 0) {
+        quantity = quantity - 1;
+    }
+    quantityContainer.textContent = `Quantity: ${quantity}`;
+});
+console.log('Sugar + button was clicked')
+
+
